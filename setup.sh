3f6691902c8cb1f6234e0ca07915a7be92907e91
root <root@Ubuntu-22.myguest.virtualbox.org>
2024-05-22 22:05:46 -0700

    add setup script for neovim

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -xe
+
+sudo apt update -y
+sudo apt upgrade -y
+
+NVIM_VERSION="v0.10.0"
+NVIM_ARCHIVE="nvim-linux64.tar.gz"
+
+NVIM_URL="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/${NVIM_ARCHIVE}"
+INSTALL_DIR="/opt/nvim-linux64"
+SYMLINK_PATH="/usr/local/bin/nvim"
+
+echo "Downloading Neovim..."
+wget $NVIM_URL -O $NVIM_ARCHIVE
+
+echo "Extracting Neovim..."
+tar -xzvf $NVIM_ARCHIVE 
+
+echo "Moving Neovim to /opt..."
+sudo mv nvim-linux64 $INSTALL_DIR
+
+echo "Creating symbolic link..."
+sudo ln -sf $INSTALL_DIR/bin/nvim $SYMLINK_PATH
+
+echo "Cleaning up..."
+rm -rf $NVIM_ARCHIVE
+
+echo "Verifying Neovim installation..."
+nvim --version
