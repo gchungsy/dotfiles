fea91609bc55f3ae097b115f4fe4c67967f685df
root <root@Ubuntu-22.myguest.virtualbox.org>
2024-05-22 23:34:14 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ NVIM_ARCHIVE="nvim-linux64.tar.gz"
 NVIM_URL="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/${NVIM_ARCHIVE}"
 INSTALL_DIR="/opt/nvim-linux64"
 SYMLINK_PATH="/usr/local/bin/nvim"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 
 echo "Downloading Neovim..."
 wget $NVIM_URL -O $NVIM_ARCHIVE
@@ -23,6 +24,9 @@ sudo mv nvim-linux64 $INSTALL_DIR
 
 echo "Creating symbolic link..."
 sudo ln -sf $INSTALL_DIR/bin/nvim $SYMLINK_PATH
+
+
+mkdir -p "$XDG_CONFIG_HOME/nvim"
 
 echo "Cleaning up..."
 rm -rf $NVIM_ARCHIVE
